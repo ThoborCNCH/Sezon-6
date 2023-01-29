537b1c22dedb652cc930b8af1a65e599a43e7468
EnderReal <66971229+EnderReal@users.noreply.github.com>
2023-01-29 22:12:47 +0200

    pozitii aliniate virtual

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Amin/NU_AM_CASA.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.Amin;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+public class NU_AM_CASA {
+
+    public static Pose2d START_DR_RED_BLUE_UPDATE = new Pose2d(36, -61, Math.toRadians(90));
+    public static Pose2d PRE_POSITION_DR_RED_BLUE_UPDATE = new Pose2d(35, -12, Math.toRadians(90));
+    public static Vector2d JUNCTION_PUNE_DR_RED_BLUE_VECTOR_UPDATE = new Vector2d(24, -3.5);
+    public static Pose2d PARK_LEFT_DR_RED_BLUE_UPDATE = new Pose2d(12, -12, Math.toRadians(90));
+    public static Pose2d PARK_RIGHT_DR_RED_BLUE_UPDATE = new Pose2d(60, -12, Math.toRadians(90));
+
+
+
+    public static Pose2d START_ST_RED_BLUE_UPDATE = new Pose2d(-36, -61, Math.toRadians(90));
+    public static Pose2d PRE_POSITION_ST_RED_BLUE_UPDATE = new Pose2d(-35, -12, Math.toRadians(90));
+    public static Vector2d JUNCTION_PUNE_ST_RED_BLUE_VECTOR_UPDATE = new Vector2d(-24, -3.5);
+    public static Pose2d PARK_LEFT_ST_RED_BLUE_UPDATE = new Pose2d(-12, -12, Math.toRadians(90));
+    public static Pose2d PARK_RIGHT_ST_RED_BLUE_UPDATE = new Pose2d(-60, -12, Math.toRadians(90));
+
+}
